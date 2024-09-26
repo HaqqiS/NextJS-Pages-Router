50a9298808652736dd0f7fac95a16c60e73e4d2f
“haqqi <haqqi30sukmara@gmail.com>
2024-09-26 20:34:23 +0800

    v1.2 pages & layouts

---
+++ src/pages/setting/app.tsx
@@ -0,0 +1,9 @@
+const AppSettingPage = () => {
+    return (
+        <div>
+            <h1>app setting</h1>
+        </div>
+    );
+};
+
+export default AppSettingPage;
---
+++ src/pages/setting/user/password/index.tsx
@@ -0,0 +1,9 @@
+const UserPasswordSettingPage = () => {
+    return (
+        <div>
+            <h1>user password setting</h1>
+        </div>
+    );
+};
+
+export default UserPasswordSettingPage;
