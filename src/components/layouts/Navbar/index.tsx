86a48bf25a7bfb4b72da23edfba9e278a694d4b9
“haqqi <haqqi30sukmara@gmail.com>
2024-10-06 20:59:25 +0800

    v1.13 setup next-auth

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,9 +1,29 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+    const { data }: any = useSession();
     return (
         <div className={styles.navbar}>
             <p className="big">Navbar</p>
+            <div className="flex gap-2 items-center">
+                <p>{data && data.user.fullname}</p>
+                {data ? (
+                    <button
+                        className="btn rounded-lg bg-gray-900 py-2 px-4 text-white"
+                        onClick={() => signOut()}
+                    >
+                        Sign Out
+                    </button>
+                ) : (
+                    <button
+                        className="btn rounded-lg bg-gray-900 py-2 px-4 text-white"
+                        onClick={() => signIn()}
+                    >
+                        Sign In
+                    </button>
+                )}
+            </div>
         </div>
     );
 };
